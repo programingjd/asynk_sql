adb9e5e1f1ad32481123e24b6529514331e05169
Jerome David <programingjd@gmail.com>
2018-10-05 17:59:58 +0200

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,15 +32,6 @@ dependencies {
   testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.0")
 }
 
-dependencies {
-  compile(kotlin("stdlib-jdk8"))
-  implementation("info.jdavid.asynk:core:0.0.0.16.2")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.27.0")
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.0")
-  testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.0")
-}
-
-
 tasks.compileKotlin {
   kotlinOptions {
     jvmTarget = "1.8"
